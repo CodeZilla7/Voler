a7faf0e253ccbf91af8ab7501daa39a32d878dc5
Eton Otieno <etonotieno@gmail.com>
2018-11-20 16:56:45 +0300

    Set up the Navigation library

--- app/src/main/java/com/eton/voler/ui/MainActivity.kt
+++ app/src/main/java/com/eton/voler/ui/MainActivity.kt
@@ -27,6 +27,8 @@ package com.eton.voler.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.eton.voler.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -36,6 +38,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        val navController = findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
+    }
 }
