a40a8c8da8fe400c6ce69b83de6006b98d544699
Eton Otieno <etonotieno@gmail.com>
2018-11-20 19:18:08 +0300

    Separate Authentication and the Flight Service

--- app/src/main/java/com/eton/voler/api/AuthService.kt
+++ app/src/main/java/com/eton/voler/api/AuthService.kt
@@ -25,22 +25,21 @@
 
 package com.eton.voler.api
 
+import com.eton.voler.BuildConfig
+import com.eton.voler.api.model.LufthansaToken
 import com.eton.voler.api.model.ScheduleResponse
 import kotlinx.coroutines.Deferred
-import retrofit2.http.GET
-import retrofit2.http.HeaderMap
-import retrofit2.http.Query
+import retrofit2.Call
+import retrofit2.http.*
 
-interface FlightService{
+interface AuthService {
 
-    @GET(Constants.FLIGHT_SCHEDULES)
-    fun getFlightSchedules(
-        @HeaderMap header: Map<String, String>,
-        @Query("origin") origin: String,
-        @Query("destination") destination: String,
-        @Query("fromDateTime") departureDate: String,
-        @Query("directFlights") directFlights: Int = 0,
-        @Query("limit") limit: String = "25",
-        @Query("offset") offset: String
-    ): Deferred<ScheduleResponse>
+    @FormUrlEncoded
+    @POST(Constants.TOKEN_ENDPOINT)
+    fun getToken(
+        @Field("client_id") clientId: String = BuildConfig.LUFTHANSA_API_KEY,
+        @Field("client_secret") clientSecret: String = BuildConfig.LUFTHANSA_SECRET,
+        @Field("grant_type") grantType: String = "client_credentials"
+    ): Deferred<LufthansaToken>
+
 }
